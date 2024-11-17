0bb6056ca9ec6720e9fc2d1354f9fa5a4132948a
Prashant <ojha.pkumar@gmail.com>
2024-11-17 22:41:38 +0530

    folder setup + setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical Chatbot',
+    author='Prashant Kumar Ojha',
+    author_email='ojha.pkumar@gmail.com',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[]
+)
